3decf0c0c09a126551ae0022f11ff86e89c99ca1
Bartosz Wilki <b.wilki0109@gmail.com>
2022-05-25 08:21:37 +0200

    Prepare global styles

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 function App() {
   return (
     <MainLayout>
-      
+
     </MainLayout>
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import store from '../src/redux/store';
 import App from './App';
 
 import './styles/normalize.scss';
+import './styles/global.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
